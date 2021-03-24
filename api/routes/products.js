28e8f09cdd8ae638f2aa6986b5b96a336e59de20
deborolon <debora.nrolon@gmail.com>
2021-03-24 19:04:34 -0300

    updated products

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,85 +1,86 @@
-const app = require('../app');
-const dbConnection = require('../server');
+const app = require("../app");
+const sequelize = require("../server");
+const authMiddleware = require("../middlewares/auth");
+const adminMiddleware = require("../middlewares/admin");
 
 //GET ALL
-app.get('/products', async  (req, res) => {
-    try{
-        let data = await dbConnection.sq.query(
-            'SELECT * FROM products',
-            {
-                type: dbConnection.sq.QueryTypes.SELECT
-            })
-        res.send(data);
-    } catch(err){
-        console.log(err);
-        res.send(err);
-    }
+app.get("/", authMiddleware, async (req, res) => {
+  try {
+    let data = await sequelize.query("SELECT * FROM products", {
+      type: sequelize.QueryTypes.SELECT,
+    });
+    res.send(data);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 //GET BY ID
-app.get('/products/:id', async  (req, res) => {
-    try{
-        let data = await dbConnection.sq.query(
-            'SELECT * from products WHERE id = :id',
-            {
-                replacements: {id: req.params.id}
-            })
-        res.send(data);
-    } catch(err){
-        console.log(err);
-        res.send(err);
-    }
+app.get("/products/:id", authMiddleware, async (req, res) => {
+  try {
+    let data = await sequelize.query("SELECT * from products WHERE id = :id", {
+      replacements: { id: req.params.id },
+      type: sequelize.QueryTypes.SELECT,
+    });
+    res.send(data[0]);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 //NEW
-app.post('/products', async (req, res) => {
-    try{
-        const { description, picture, price} = req.body;
+app.post("/products", authMiddleware, adminMiddleware, async (req, res) => {
+  try {
+    const { description, picture, price } = req.body;
 
-        await dbConnection.sq.query(
-            `INSERT into products
+    await sequelize.query(
+      `INSERT into products
                 (description, picture, price)
             VALUES
                 (:description, :picture, :price)`,
-        { replacements: {description, picture, price} });
+      { replacements: { description, picture, price } }
+    );
 
-        res.sendStatus(200);
-    } catch (err){
-        res.send(err);
-    }
+    res.status(200).send("Product has been added.");
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 //UPDATE BY ID
-app.put('/products/:id', async (req, res) => {
-    try{
-        const { description, picture, price} = req.body;
+app.put("/products/:id", authMiddleware, adminMiddleware, async (req, res) => {
+  try {
+    const { description, picture, price } = req.body;
 
-    await dbConnection.sq.query(
-        `UPDATE products SET
+    await sequelize.query(
+      `UPDATE products SET
             description = ?,
             picture = ?,
             price = ?
         WHERE id = ${req.params.id}`,
-        { replacements: [description, picture, price] });
+      { replacements: [description, picture, price] }
+    );
 
-    res.sendStatus(200);
-    } catch (err){
-        res.send(err);
-    }
+    res.status(200).send("The product has been modified.");
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 //DELETE BY ID
-app.delete('/products/:id', async (req, res) => {
-    try{
-        await dbConnection.sq.query(
-            `DELETE from products WHERE id = :id`,
-            {
-                replacements: {id: parseInt(req.params.id)}
-            },
-        );
-    
-        res.sendStatus(200);
-    } catch (err){
-        res.send(err);
+app.delete(
+  "/products/:id",
+  authMiddleware,
+  adminMiddleware,
+  async (req, res) => {
+    try {
+      await sequelize.query(`DELETE from products WHERE id = :id`, {
+        replacements: { id: parseInt(req.params.id) },
+      });
+
+      res.status(200).send("The product has been removed.");
+    } catch (err) {
+      res.send(err);
     }
-});
+  }
+);
