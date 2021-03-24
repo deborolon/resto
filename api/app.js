9221e41819423264e58388acb55f1b8da00e382d
deborolon <debora.nrolon@gmail.com>
2021-03-24 19:52:44 -0300

    app & server

--- api/app.js
+++ api/app.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8001;
-const { QueryTypes } = require('sequelize');
+const { QueryTypes } = require("sequelize");
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`)
-})
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+app.use(bodyParser.json());
 
 module.exports = app;
